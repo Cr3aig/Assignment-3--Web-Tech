188d29637bd582528995599e8536d473ccfd74d0
Cr3aig <74608032+Cr3aig@users.noreply.github.com>
2021-06-18 16:03:36 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+        function myFuction()
+        {
+            var change = document.getElementsByClassName("lion");
+        
+        for (var i = 0; i <change.length;i++)
+        {
+            change[i].style.backgroundColor = "black";
+            change[i].style.color = "white";
+        }
+        
+        }
